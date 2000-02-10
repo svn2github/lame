0f6e33c0825ef440a3a979c64f544aeb75cade39
(no author) <(no author)@981c7233-bbb0-e711-95a9-001517a2e1a4>
2000-02-10 06:15:51 +0000

    This commit was manufactured by cvs2svn to create tag 'lame3_62beta'.
    git-svn-id: https://svn.code.sf.net/p/lame/svn@220 981c7233-bbb0-e711-95a9-001517a2e1a4

---
+++ tags/lame3_62beta/lame/fft.h
@@ -0,0 +1,10 @@
+#ifndef FFT_H
+#define FFT_H
+
+#include "encoder.h"
+
+void fft_long(FLOAT *x_real, FLOAT energy[HBLKSIZE], int, short **);
+void fft_short(FLOAT *x_real, FLOAT energy_s[3][HBLKSIZE_s], int, short **);
+void init_fft(void);
+
+#endif
