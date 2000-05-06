1ea5aff48f2995ce378e143b2d3017f236d61539
(no author) <(no author)@981c7233-bbb0-e711-95a9-001517a2e1a4>
2000-05-06 22:07:06 +0000

    This commit was manufactured by cvs2svn to create tag 'test39'.
    git-svn-id: https://svn.code.sf.net/p/lame/svn@578 981c7233-bbb0-e711-95a9-001517a2e1a4

---
+++ tags/test39/lame/reservoir.h
@@ -0,0 +1,16 @@
+/**********************************************************************
+ * ISO MPEG Audio Subgroup Software Simulation Group (1996)
+ * ISO 13818-3 MPEG-2 Audio Encoder - Lower Sampling Frequency Extension
+ *
+ **********************************************************************/
+
+
+#ifndef RESERVOIR_H
+#define RESERVOIR_H
+
+int ResvFrameBegin( lame_global_flags *gfp,III_side_info_t *l3_side, int mean_bits, int frameLength );
+void ResvMaxBits(lame_global_flags *gfp, int mean_bits, int *targ_bits, int *max_bits);
+void ResvAdjust(lame_global_flags *gfp,gr_info *gi, III_side_info_t *l3_side, int mean_bits );
+void ResvFrameEnd(lame_global_flags *gfp,III_side_info_t *l3_side, int mean_bits );
+
+#endif
