8b7490e97ddc27ba642441bc0c096218e1c4d6ef
(no author) <(no author)@981c7233-bbb0-e711-95a9-001517a2e1a4>
2000-09-13 10:59:08 +0000

    This commit was manufactured by cvs2svn to create tag 'test50'.
    git-svn-id: https://svn.code.sf.net/p/lame/svn@1082 981c7233-bbb0-e711-95a9-001517a2e1a4

---
+++ tags/test50/lame/tables.h
@@ -0,0 +1,46 @@
+/*
+** Copyright (C) 1999 Albert L. Faber
+**  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef TABLES_H_INCLUDED
+#define TABLES_H_INCLUDED
+#include "encoder.h"
+#include "machine.h"
+extern const FLOAT8 psy_data [];
+
+#define HUFFBITS unsigned long int	// dangerous on ALPHA
+#define HTN	34
+ 
+struct huffcodetab {
+    const unsigned int    xlen; 	/*max. x-index+			      	*/ 
+    const unsigned int    linmax;	/*max number to be stored in linbits	*/
+    const HUFFBITS*       table;	/*pointer to array[xlen][ylen]		*/
+    const unsigned char*  hlen;	/*pointer to array[xlen][ylen]		*/
+};
+
+extern const struct huffcodetab ht [HTN];
+extern const struct huffcodetab ht_takehiro [HTN];
+    /* global memory block			*/
+    /* array of all huffcodtable headers	*/
+    /* 0..31 Huffman code table 0..31		*/
+    /* 32,33 count1-tables			*/
+
+extern const unsigned char t32l [];
+extern const unsigned char t33l [];
+
+#endif
