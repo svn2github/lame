794794ffed437e598322b305d7ba43f172e426af
(no author) <(no author)@981c7233-bbb0-e711-95a9-001517a2e1a4>
2000-12-12 01:11:28 +0000

    This commit was manufactured by cvs2svn to create tag 'test58c'.
    git-svn-id: https://svn.code.sf.net/p/lame/svn@1680 981c7233-bbb0-e711-95a9-001517a2e1a4

---
+++ tags/test58c/lame/configMS.h
@@ -0,0 +1,21 @@
+/* The number of bytes in a int.  */
+#define SIZEOF_INT 4
+
+/* The number of bytes in a unsigned long.  */
+#define SIZEOF_UNSIGNED_LONG 4
+
+/* Name of package */
+#define PACKAGE "lame"
+
+/* Version number of package */
+#define VERSION "3.88"
+
+/* Define if compiler has function prototypes */
+#define PROTOTYPES 1
+
+/* enable VBR bitrate histogram */
+#define BRHIST 1
+
+/* IEEE754 compatible machine */
+#define TAKEHIRO_IEEE754_HACK 1
+
